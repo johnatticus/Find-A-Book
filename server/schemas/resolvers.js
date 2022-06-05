a5b5156b1bf01520f134c553d2dd31404fba1a55
John Hysong <johnahysong@gmail.com>
2022-06-04 20:42:36 -0400

    worked on resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,34 @@
+// define the query and mutation functionality to work with the Mongoose models.
+// HINT: use the functionality in the user-controller.js as a guide
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+  Query: {
+    user: async () => {
+      return User.find({});
+    },
+    matchups: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Matchup.find(params);
+    },
+  },
+  Mutation: {
+    createMatchup: async (parent, args) => {
+      const matchup = await Matchup.create(args);
+      return matchup;
+    },
+    createVote: async (parent, { _id, techNum }) => {
+      const vote = await Matchup.findOneAndUpdate(
+        { _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return vote;
+    },
+  },
+};
+
+module.exports = resolvers;
