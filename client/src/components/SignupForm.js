1f5a9922052185b7cb72b3781345843b07ba5d8e
John Hysong <johnahysong@gmail.com>
2022-06-04 21:19:04 -0400

    worked on components files from client

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+
+// Replace the addUser() functionality imported from API file with ADD_USER mutation functionality
 
 const SignupForm = () => {
   // set initial form state
@@ -28,7 +31,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await ADD_USER(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -70,7 +70,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await SAVE_BOOK(bookToSave, token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
