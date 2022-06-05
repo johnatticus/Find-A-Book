61c8449f9c5e9a8373684e63691b9ae64ed86fa4
John Hysong <johnahysong@gmail.com>
2022-06-04 23:27:13 -0400

    worked on typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,34 +2,43 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Profile {
-    _id: ID
-    name: String
-    email: String
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
     # There is now a field to store the user's password
-    password: String
-    skills: [String]!
+  }
+
+  type: Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String!
+    image: String!
+    link: String!
   }
 
   # Set up an Auth type to handle returning data from a profile creating or user login
   type Auth {
     token: ID!
-    profile: Profile
+    user: User
   }
 
   type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
+    me: User
   }
 
   type Mutation {
     # Set up mutations to handle creating a profile or logging into a profile and return Auth type
-    addProfile(name: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile(profileId: ID!): Profile
-    removeSkill(profileId: ID!, skill: String!): Profile
+    saveBook(authors: Array!, description: String!, title: String!, bookId: String!, image: String!, link: String!): User
+    removeBook(bookId: String!): User
+    # addUser(profileId: ID!, skill: String!): Profile
+    # removeProfile(profileId: ID!): Profile
+    # removeSkill(profileId: ID!, skill: String!): Profile
   }
 `;
 
