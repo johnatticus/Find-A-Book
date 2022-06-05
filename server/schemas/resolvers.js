b9626057981c54fc60e83001d4ca1f55cbe4aee3
John Hysong <johnahysong@gmail.com>
2022-06-04 22:53:13 -0400

    worked on resolvers. probably broken

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,29 +7,72 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // SHE HAD THIS:
-    me: async(parent, args, context) => {
-    // user: async () => {
-      return User.find({});
+    // TA HAD THIS:
+    me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id }).populate('books');
+        }
+        throw new AuthenticationError('You need to be logged in!');
     },
-    matchups: async (parent, { _id }) => {
+    Book: async (parent, { _id }) => {
       const params = _id ? { _id } : {};
-      return Matchup.find(params);
+      return Book.find(params);
     },
   },
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('No user found with this email address');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
     },
+    saveBook: async (parent, { bookId, image, title, authors, description }) => {
+        return User.findOneAndUpdate(
+          { _id: userId },
+          {
+            $addToSet: { savedBooks: book._id },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+    },
+    deleteBook: async (parent, { _id }, context) => {
+          await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { books: book._id } }
+          );
+        //   return thought;
+        }
+    // createMatchup: async (parent, args) => {
+    //   const matchup = await Matchup.create(args);
+    //   return matchup;
+    // },
+    // createVote: async (parent, { _id, techNum }) => {
+    //   const vote = await Matchup.findOneAndUpdate(
+    //     { _id },
+    //     { $inc: { [`tech${techNum}_votes`]: 1 } },
+    //     { new: true }
+    //   );
+    //   return vote;
+    // },
   },
 };
 
